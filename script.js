3488fabc50e22ede55d4527f5dd69a5d82d7135e
Aditya Raj <adityaraj93107@gmail.com>
2025-07-31 20:28:41 +0530

    added logic for minicircle mouse follower

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower() {
+    window.addEventListener('mousemove', (event) => {
+        //clientX and clientY gives information where is the mouse cursor
+        document.querySelector('#minicircle').style.transform = `translate(${event.clientX}px, ${event.clientY}px)`
+    });
+}
+
+circleMouseFollower();
